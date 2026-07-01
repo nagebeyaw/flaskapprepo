35b1fb5614a3470c24bc31b3f2e945d8ecf7f66d
Nathaniel Gebeyaw <nagebeyaw@gmail.com>
2026-07-01 21:23:35 +0000

    added auto deployment

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from flask import Flask
-
+import git
+from flask import Flask, render_template, url_for, flash, redirect, request
 app = Flask(__name__)
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/nagebeyaw/flaskapprepo')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 @app.route('/')
 def home():
