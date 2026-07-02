8ae843ab916c1bd82576a64bcc82aae9998e6bd7
Nathaniel Gebeyaw <nagebeyaw@gmail.com>
2026-07-02 01:41:29 +0000

    added db and orm setup

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 import git
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, url_for, flash, redirect, request
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  password = db.Column(db.String(60), nullable=False)
+
+  def __repr__(self):
+    return f"User('{self.username}', '{self.email}')"
+
+with app.app_context():
+  db.create_all()
+
+if form.validate_on_submit(): #already in your code file
+  user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+  db.session.add(user)
+  db.session.commit()
+
 @app.route("/update_server", methods=['POST'])
 def webhook():
     if request.method == 'POST':
